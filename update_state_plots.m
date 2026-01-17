6d624c5eb19e5b11e67b9d86cdbbf4edf52ef794
Ashraf AlAssi <ashraf@rachis.co>
2026-01-17 09:08:31 +0300

    Remove deprecated files related to cart-pole dynamics, example usage, parameter retrieval, visualization, and simulation methods to streamline the codebase.

--- update_state_plots.m
+++
@@ -1,8 +0,0 @@
-function update_state_plots(lines, state, time)
-% Push new data to the animated lines
-for i = 1:4
-    addpoints(lines(i), time, state(i));
-end
-% Force update
-drawnow limitrate;
-end
